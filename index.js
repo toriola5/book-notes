44fe8770a11583c609c56e42ba2bf081864591ca
David <toriola.dolapodavid@gmail.com>
2025-11-01 15:01:37 +0000

    Editing index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import booksRouter from './routes/book.js';
-import bookUpdate from './routes/bookAction.js'
+import bookUpdate from './routes/bookAction.js';
 
 
 const app = express();
@@ -9,7 +9,7 @@ const port = 3000;
 app.use(express.static("public"));
 app.set("view engine" , "ejs");
 app.use(express.urlencoded({extended : true}));
-app.use(express.json())
+app.use(express.json());
 
 app.use ("/" , booksRouter);
 app.use("/books", bookUpdate);
